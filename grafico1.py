a96b52153effd5112d5192b10825db51d9360cb5
jmorales84 <163792091+jmorales84@users.noreply.github.com>
2025-05-29 19:38:56 -0600

    Add files via upload

---
+++ grafico1.py
@@ -0,0 +1,6 @@
+from tkinter import*
+raiz=Tk() #la variable raiz toma el valor de modo grafico (ventana)
+raiz.title("Tecnologico")
+#raiz.resizable(True,True) # se puede apliar horizontal y vertical
+raiz.geometry("650x400") # la vantana esta definida 650 por 400 pixeles 
+raiz.mainloop()
